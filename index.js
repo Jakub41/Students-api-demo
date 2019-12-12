5d65c824120ed83fceb55d3dcfbfb881894691d2
jakub41 <lemiszewski@gmx.com>
2019-12-12 21:03:25 +0100

    Implemented basic server module

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const { port } = require('./src/config/config');
-console.log(`Your port is ${port}`);
+// Env variables from config.js
+const  { port } = require("./src/config/config");
+
+// Express lib
+const express = require("express");
+
+// Morgan lib logger middleware https://www.npmjs.com/package/morgan
+// To log app activities a good way to keep track of what is going on
+// Useful also to the bug issues when an exception come ups
+const morgan = require("morgan");
+
+// Defining the server/app
+const server = express();
+
+// We use Morgan to log our server
+// "tiny" The minimal output of the log the default light param
+server.use(morgan("tiny"));
+
+// Starting the server on env port
+server.listen(`${port}`, () => {
+    // Showing a message to the console informing on which port is running
+    console.log(`Server is running on port ${port}`);
+});
